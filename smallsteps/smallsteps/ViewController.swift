9be78a45aa8f448828b1657c7be8c527f03e72da
jinsun91 <jinsunpark917@gmail.com>
2018-05-31 16:38:50 +0100

    added current location map feature

--- smallsteps/smallsteps/ViewController.swift
+++ smallsteps/smallsteps/ViewController.swift
@@ -12,14 +12,31 @@ import MapKit
 import Alamofire
 import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet var map: MKMapView!
     @IBOutlet var menuButton: UIButton!
-    @IBOutlet var MapView: MKMapView!
     @IBOutlet weak var searchBar: UISearchBar!
+    
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        let span: MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let myLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region: MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        map.setRegion(region, animated: true)
+        
+        self.map.showsUserLocation = true
+    }
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
         
         Alamofire.request("http://146.169.45.120:8080/smallsteps/greeting").responseJSON { (responseData) -> Void in
             if((responseData.result.value) != nil) {
