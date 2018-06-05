3f7d9c75267c3e23fc4a6a530c2076319450d242
jinsun91 <jinsunpark917@gmail.com>
2018-06-05 13:30:29 +0100

    Added Eureka form creator to create group page

---
+++ smallsteps/smallsteps/CreateGroupVC.swift
@@ -0,0 +1,77 @@
+//
+//  CreateGroupTVC.swift
+//  smallsteps
+//
+//  Created by Jin Sun Park on 05/06/2018.
+//  Copyright © 2018 group29. All rights reserved.
+//
+
+import UIKit
+
+import Eureka
+
+class CreateGroupVC: FormViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        form +++ Section("Section1")
+            <<< TextRow(){ row in
+                row.title = "Text Row"
+                row.placeholder = "Enter text here"
+            }
+            <<< PhoneRow(){
+                $0.title = "Phone Row"
+                $0.placeholder = "And numbers here"
+            }
+            +++ Section("Section2")
+            <<< DateRow(){
+                $0.title = "Date Row"
+                $0.value = Date(timeIntervalSinceReferenceDate: 0)
+        }
+    }
+}
+
+//class CreateGroupTVC: UITableViewController {
+//
+//    enum ProfileSections: Int {
+//        case Profile = 0,
+//        Bio, // new section
+//        Info,
+//        Friends
+//    }
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Uncomment the following line to preserve selection between presentations
+//        // self.clearsSelectionOnViewWillAppear = false
+//
+//        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+//        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//
+//    // MARK: - Table view data source
+//
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        return 1
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 2
+//    }
+//
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+//
+//        cell.textLabel?.text = "Test"
+//
+//        return cell
+//    }
+//
+//
+//}
