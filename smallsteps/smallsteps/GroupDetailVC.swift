cee5cb7cc0617de6daf1374377e2f73a6651dada
jinsun91 <jinsunpark917@gmail.com>
2018-06-11 17:57:39 +0100

    Working on group detail page

--- smallsteps/smallsteps/GroupDetailVC.swift
+++ smallsteps/smallsteps/GroupDetailVC.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class GroupDetailVC: UIViewController {
-    @IBOutlet weak var groupName: UILabel!
+class GroupDetailVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        groupName.text = yourGroupNames[myIndex]
         // Do any additional setup after loading the view.
     }
 
@@ -23,15 +26,10 @@ class GroupDetailVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "groupDetailCell", for: indexPath) as! GroupDetailTableViewCell
+        cell.groupNameLabel.text = yourGroupNames[myIndex]
+        return cell
     }
-    */
-
 }
